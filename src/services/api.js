9abda0c90545a2147081500ca1f7a680cab74d1d
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2019-09-19 20:13:37 -0300

    configurado api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://prossdev.herokuapp.com/',
+  baseURL: 'https://getinfo-api-frontend.herokuapp.com',
 });
 
 api.interceptors.request.use((req) => {
