2c2192e928b60d20301c65cb88cedda78af48de6
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2019-09-20 00:12:35 -0300

    Modulo adicionar nova tarefa

--- src/screens/tarefas/tarefas-nova/index.js
+++ src/screens/tarefas/tarefas-nova/index.js
@@ -1,54 +1,52 @@
-import React, { useState } from 'react';
-import { api } from '../../../services';
+import React, { useState } from "react";
+import { api } from "../../../services";
 
 function Main(props) {
   const {
-    history: { goBack, push },
+    history: { goBack, push }
   } = props;
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState({
+    task: ""
+  });
   const handleSave = async () => {
     console.log(task);
     try {
-      await api.post('/tasks', task);
-      push('/tarefas');
+      await api.post("tasks", task);
+      push("/tarefas");
     } catch (e) {
-      console.log('teste');
+      console.log(e);
     }
   };
   return (
     <div className="modal is-active">
-      <div className="column">
-        <div className="modal-background" />
-        <div className="modal-card">
-          <div className="field">
-            <header className="modal-card-head">
-              <p className="modal-card-title">Vamos lá</p>
-            </header>
-            <section className="modal-card-body">
-              <div className="content">
-                <p>Digite as informações sobre as tarefas</p>
-              </div>
-              <input
-                className="input"
-                type="password"
-                value={task}
-                onChange={e => setTask(e.target.value)}
-              />
-            </section>
-            <footer className="modal-card-foot has-text-right">
-              <button
-                type="submit"
-                className="button is-danger"
-                onClick={handleSave}
-              >
-                Salvar
-              </button>
-              <button type="button" className="button" onClick={goBack}>
-                Cancelar
-              </button>
-            </footer>
+      <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Vamos lá</p>
+        </header>
+        <section className="modal-card-body">
+          <div className="content">
+            <p>Digite a descrição da tarefa:</p>
           </div>
-        </div>
+          <input
+            className="input"
+            type="text"
+            value={task.task}
+            onChange={e => setTask({ ...task, task: e.target.value })}
+          />
+        </section>
+        <footer className="modal-card-foot has-text-right">
+          <button
+            type="submit"
+            className="button is-danger"
+            onClick={handleSave}
+          >
+            Salvar
+          </button>
+          <button type="button" className="button" onClick={goBack}>
+            Cancelar
+          </button>
+        </footer>
       </div>
     </div>
   );
