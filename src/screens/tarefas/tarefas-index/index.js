691ccf4f1897b9b01ebe087f8a7493db7fff6ae5
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2019-09-19 22:30:36 -0300

    Tela de visualizar tarefas com inicio da integração

--- src/screens/tarefas/tarefas-index/index.js
+++ src/screens/tarefas/tarefas-index/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { api } from "../../../services";
 
 function Main() {
-  return (
-    <div id="tarefa-index" className="hero is-fullheight">
-      <div>
-        <p>teste</p>
-      </div>
+  const [tasks, setTasks] = useState({});
+  const hangleGetTasks = async () => {
+    const { data } = await api.get("/tasks");
+    console.log(data);
+    setTasks(data);
+  };
+  useEffect(() => {
+    hangleGetTasks();
+  }, []);
+
+  const renderActions = (id = 0) => (
+    <div className="buttons">
+      <Link to="/" className="button is-small is-primary">
+        Editar
+      </Link>
+      <Link to="/" className="button is-small is-primary">
+        Excluir
+      </Link>
     </div>
+  );
+
+  return (
+    <main className="column">
+      <div>
+        <h1 className="sub-title is-0">Suas Tarefas Cadastradas</h1>
+        <h1 className="title">TASKS</h1>
+        <div>
+          <div className="level-right">
+            <div>
+              <Link to="/tarefas/novo" className="button is-primary">
+                Nova Tarefa
+              </Link>
+            </div>
+          </div>
+        </div>
+        <table className="table is-fullwidth is-hoverable">
+          <thead>
+            <th>titulo</th>
+          </thead>
+          <tbody>
+            {Array(9).map(item => (
+              <tr>
+                <td>titulo</td>
+                <td>{renderActions()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
   );
 }
 export default Main;
